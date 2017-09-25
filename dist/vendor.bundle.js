ed26d7584d9d697c01eb6d64088ae5f04b2673f2
Animesh Mathur <animeshmathur92@gmail.com>
2017-09-25 18:46:43 +0530

    included add member form + edit member form

--- dist/vendor.bundle.js
+++ dist/vendor.bundle.js
@@ -51730,17 +51730,17 @@ function transition$$1(stateChangeExpr, steps) {
 /* unused harmony export MinLengthValidator */
 /* unused harmony export PatternValidator */
 /* unused harmony export RequiredValidator */
-/* unused harmony export FormBuilder */
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return FormBuilder; });
 /* unused harmony export AbstractControl */
 /* unused harmony export FormArray */
-/* unused harmony export FormControl */
-/* unused harmony export FormGroup */
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "b", function() { return FormControl; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "c", function() { return FormGroup; });
 /* unused harmony export NG_ASYNC_VALIDATORS */
 /* unused harmony export NG_VALIDATORS */
 /* unused harmony export Validators */
 /* unused harmony export VERSION */
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return FormsModule; });
-/* unused harmony export ReactiveFormsModule */
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "d", function() { return FormsModule; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "e", function() { return ReactiveFormsModule; });
 /* unused harmony export ɵba */
 /* unused harmony export ɵz */
 /* unused harmony export ɵx */
--- src/app/members-directory/members-directory.module.ts
+++ src/app/members-directory/members-directory.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { MembersDirectoryRoutingModule } from './members-directory-routing.module';
 import { MembersListComponent } from './members-list/members-list.component';
+import { AddMemberFormComponent } from './add-member-form/add-member-form.component';
+import { EditMemberFormComponent } from './edit-member-form/edit-member-form.component';
 
 @NgModule({
 	imports: [
 		CommonModule,
+		ReactiveFormsModule,
 		MembersDirectoryRoutingModule
 	],
-	declarations: [MembersListComponent],
+	declarations: [MembersListComponent, AddMemberFormComponent, EditMemberFormComponent],
 	exports: [MembersListComponent]
 })
 export class MembersDirectoryModule { }
